c0588997b03535953fee57b232bca42b60b467f7
Sason Braha <sasonbraha@gmail.com>
2019-03-16 12:54:38 +0200

    Updated console.log, added prettier

--- src/ts/global.ts
+++ src/ts/global.ts
@@ -3,25 +3,32 @@ const content = document.querySelector('.content') as HTMLElement;
 const loaderGroup = document.querySelector('.group') as HTMLElement;
 
 window.onload = () => {
-  // Animate { .site-loader } Closing
-  requestAnimationFrame(() => {
-    loaderGroup.style.opacity = '0';
-    loaderGroup.addEventListener('transitionend', () => {
-      siteLoader.classList.add('loaded');
-      content.style.visibility = 'visible';
-    });
-  });
+	// Animate { .site-loader } Closing
+	requestAnimationFrame(() => {
+		loaderGroup.style.opacity = '0';
+		loaderGroup.addEventListener('transitionend', () => {
+			siteLoader.classList.add('loaded');
+			content.style.visibility = 'visible';
+		});
+	});
 
-  // Lazy-Load Images 
-  const images = document.querySelectorAll<HTMLImageElement>('.lazy-load');
-  images.forEach(image => {
-    const imgEl = new Image();
-    imgEl.onload = () => {
-      image.src = image.getAttribute('data-src');
-      image.style.filter = 'blur(0)';
-    }
-    imgEl.src = image.getAttribute('data-src');
-  });
+	// Lazy-Load Images
+	const images = document.querySelectorAll<HTMLImageElement>('.lazy-load');
+	images.forEach(image => {
+		const imgEl = new Image();
+		imgEl.onload = () => {
+			image.src = image.getAttribute('data-src');
+			image.style.filter = 'blur(0)';
+		};
+		imgEl.src = image.getAttribute('data-src');
+	});
 
-  console.log('Check out the source code: https://github.com/SasonBraha/Portfolio');
-}
+	console.log(
+		`%cCheck out the source code: https://github.com/SasonBraha/Portfolio`,
+		'font-size: 17px; font-weight: bold; color: #1976d2'
+	);
+	console.log(
+		`%cCheck out my resume (Built with Node.js): https://github.com/SasonBraha/Resume`,
+		'font-size: 17px; font-weight: bold; color: #1976d2'
+	);
+};
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,131 +1,146 @@
 import '../scss/main.scss';
 import './global.ts';
 import './canvas.ts';
-//@ts-ignore
-import { areClipPathShapesSupported, isBackgroundClipTextSupported, copyToClipboard } from './utils.ts';
+import {
+	areClipPathShapesSupported,
+	isBackgroundClipTextSupported,
+	copyToClipboard
+	//@ts-ignore
+} from './utils.ts';
 
 // Handle Contact Form Submission
 const contactForm = document.forms['contact-form'];
 const submitBtn = document.querySelector('.contact__submit-btn');
 contactForm.onsubmit = e => {
-  e.preventDefault();
-  const formData = {
-    fullName: contactForm['fullName'].value.trim(),
-    email: contactForm['email'].value.trim(),
-    message: contactForm['message'].value.trim(),
-  }
+	e.preventDefault();
+	const formData = {
+		fullName: contactForm['fullName'].value.trim(),
+		email: contactForm['email'].value.trim(),
+		message: contactForm['message'].value.trim()
+	};
 
-  // Clear Previous Errors (If Any)
-  document.querySelectorAll('.contact__validation-error').forEach(el => el.remove());
-  document.querySelectorAll('.contact__contentEditable').forEach(el => el.classList.remove('invalid'));
+	// Clear Previous Errors (If Any)
+	document
+		.querySelectorAll('.contact__validation-error')
+		.forEach(el => el.remove());
+	document
+		.querySelectorAll('.contact__contentEditable')
+		.forEach(el => el.classList.remove('invalid'));
 
-  // Show HTTP Loader
-  const httpStatus = document.querySelector('.contact__http-status');
-  httpStatus.classList.remove('success', 'error');
-  httpStatus.classList.add('in-progress');
+	// Show HTTP Loader
+	const httpStatus = document.querySelector('.contact__http-status');
+	httpStatus.classList.remove('success', 'error');
+	httpStatus.classList.add('in-progress');
 
-  // Disable Button
-  submitBtn.classList.add('disabled');
-  submitBtn.setAttribute('disabled', 'disabled');
-  
-  // Init Fetch
-  const fetchUri = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://personal-portfolio-inquiries.herokuapp.com/';
-  fetch(fetchUri, {
-    method: 'post',
-    headers: {
-      'Accept': 'application/json',
-      'Content-type': 'application/json'
-    },
-    body: JSON.stringify(formData)
-  })
-  .then(res => {
-    // Check For Too Many Requests
-    if (res.status === 429) {
-      httpStatus.classList.remove('success');
-      httpStatus.classList.add('error');
+	// Disable Button
+	submitBtn.classList.add('disabled');
+	submitBtn.setAttribute('disabled', 'disabled');
 
-      // Activate Button
-      submitBtn.classList.remove('disabled');
-      submitBtn.removeAttribute('disabled');
+	// Init Fetch
+	const fetchUri =
+		process.env.NODE_ENV === 'development'
+			? 'http://localhost:3000'
+			: 'https://personal-portfolio-inquiries.herokuapp.com/';
+	fetch(fetchUri, {
+		method: 'post',
+		headers: {
+			Accept: 'application/json',
+			'Content-type': 'application/json'
+		},
+		body: JSON.stringify(formData)
+	}).then(res => {
+		// Check For Too Many Requests
+		if (res.status === 429) {
+			httpStatus.classList.remove('success');
+			httpStatus.classList.add('error');
 
-      // Create Error Div
-      const errorDiv = document.createElement('div');
-      errorDiv.className = 'contact__validation-error u-center-text';
-      errorDiv.innerText = 'Too many resubmissions, please try again later.';
-      errorDiv.style.fontSize = '1.6rem';
-      submitBtn.parentNode.insertBefore(errorDiv, submitBtn.nextSibling);
-      
-      return;
-    }
+			// Activate Button
+			submitBtn.classList.remove('disabled');
+			submitBtn.removeAttribute('disabled');
 
-    res.json().then(data => {
-      const { success, errors } = data;
-      if (!success) {
-        // Show Error Status Icon
-        httpStatus.classList.remove('success');
-        httpStatus.classList.add('error');
-  
-        // Activate Button
-        submitBtn.classList.remove('disabled');
-        submitBtn.removeAttribute('disabled');
-  
-        // Handle Errors
-        //@ts-ignore
-        Object.entries(errors).map(([ id, { message } ]) => {
-          // Select Invalid Input
-          const invalidInput = document.getElementById(id) as HTMLInputElement;
-  
-          // Create Error Message
-          const errorSpan = document.createElement('span') as HTMLElement;
-          errorSpan.className = 'contact__validation-error';
-          errorSpan.innerText = message;
-  
-          // Append Error Message
-          invalidInput.parentNode.insertBefore(errorSpan, invalidInput.nextSibling);
-  
-          // Add { 'invalid' } Class To Invalid Input
-          invalidInput.classList.add('invalid');
-        });
-      } else {
-        // Show Success Status Icon
-        httpStatus.classList.remove('error');
-        httpStatus.classList.add('success');
-  
-        // Activate Button
-        submitBtn.classList.remove('disabled');
-        submitBtn.removeAttribute('disabled');
-  
-        // Remove Http Status Icon
-        const hideHttpStatus = () => {
-          clearTimeout(timeout);
-          timeout = setTimeout(() => {
-            httpStatus.classList.remove('success', 'error', 'in-progress');
-            httpStatus.removeEventListener('transitionend', hideHttpStatus);
-          }, 2500);
-        }
-        httpStatus.addEventListener('transitionend', hideHttpStatus);
-        let timeout;
-  
-        // Reset Form
-        e.target.reset();
-      }
-    });
-  })
-}
+			// Create Error Div
+			const errorDiv = document.createElement('div');
+			errorDiv.className = 'contact__validation-error u-center-text';
+			errorDiv.innerText = 'Too many resubmissions, please try again later.';
+			errorDiv.style.fontSize = '1.6rem';
+			submitBtn.parentNode.insertBefore(errorDiv, submitBtn.nextSibling);
+
+			return;
+		}
+
+		res.json().then(data => {
+			const { success, errors } = data;
+			if (!success) {
+				// Show Error Status Icon
+				httpStatus.classList.remove('success');
+				httpStatus.classList.add('error');
+
+				// Activate Button
+				submitBtn.classList.remove('disabled');
+				submitBtn.removeAttribute('disabled');
+
+				// Handle Errors
+				//@ts-ignore
+				Object.entries(errors).map(([id, { message }]) => {
+					// Select Invalid Input
+					const invalidInput = document.getElementById(id) as HTMLInputElement;
+
+					// Create Error Message
+					const errorSpan = document.createElement('span') as HTMLElement;
+					errorSpan.className = 'contact__validation-error';
+					errorSpan.innerText = message;
+
+					// Append Error Message
+					invalidInput.parentNode.insertBefore(
+						errorSpan,
+						invalidInput.nextSibling
+					);
+
+					// Add { 'invalid' } Class To Invalid Input
+					invalidInput.classList.add('invalid');
+				});
+			} else {
+				// Show Success Status Icon
+				httpStatus.classList.remove('error');
+				httpStatus.classList.add('success');
+
+				// Activate Button
+				submitBtn.classList.remove('disabled');
+				submitBtn.removeAttribute('disabled');
+
+				// Remove Http Status Icon
+				const hideHttpStatus = () => {
+					clearTimeout(timeout);
+					timeout = setTimeout(() => {
+						httpStatus.classList.remove('success', 'error', 'in-progress');
+						httpStatus.removeEventListener('transitionend', hideHttpStatus);
+					}, 2500);
+				};
+				httpStatus.addEventListener('transitionend', hideHttpStatus);
+				let timeout;
+
+				// Reset Form
+				e.target.reset();
+			}
+		});
+	});
+};
 
 // Check For { clip-path: polygon } Support
 const myProfleImage = document.querySelector('.about-me__image') as HTMLElement;
 if (!areClipPathShapesSupported()) {
-  myProfleImage.style.borderRadius = '50%';
+	myProfleImage.style.borderRadius = '50%';
 }
 
 // Check For { background-clip: text } Support
-const allClippedTextElements = document.querySelectorAll<HTMLElement>('.clip-text');
+const allClippedTextElements = document.querySelectorAll<HTMLElement>(
+	'.clip-text'
+);
 if (!isBackgroundClipTextSupported()) {
-  allClippedTextElements.forEach(el => {
-    el.style.color = 'black';
-    el.style.background = 'transparent'
-  });
+	allClippedTextElements.forEach(el => {
+		el.style.color = 'black';
+		el.style.background = 'transparent';
+	});
 }
 
 // Copy Email To Clipboard
@@ -134,14 +149,13 @@ const toastText = document.querySelector('.contact__toast') as HTMLElement;
 const emailAddress = 'sasonbraha@gmail.com';
 let timeout;
 toastEl.onclick = () => {
-  clearTimeout(timeout);
-  copyToClipboard(emailAddress, () => {
-    toastText.innerText = 'Email copied';
-  });
-  timeout = setTimeout(() => toastText.innerText = emailAddress, 1000);
-}
+	clearTimeout(timeout);
+	copyToClipboard(emailAddress, () => {
+		toastText.innerText = 'Email copied';
+	});
+	timeout = setTimeout(() => (toastText.innerText = emailAddress), 1000);
+};
 
 // Add Full Year To Footer
 const fullYear = document.querySelector('.full-year') as HTMLElement;
 fullYear.innerText = `${new Date().getFullYear()}`;
-
